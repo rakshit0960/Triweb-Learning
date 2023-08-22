7e54c456af9f2286d972cd85425dbf786b57664e
Rakshit Mehta <rakshit0960@gmail.com>
2023-08-22 22:34:01 +0530

    day3/forLoops loops and for loop

---
+++ JavaScript/day3/loops/forloop.js
@@ -0,0 +1,13 @@
+
+for ( /* initialization block */ let i = 0; /*conditional block*/ i <= 10; /* increment block*/ i++) 
+{
+    /* inside for loop */
+    console.log('count:', i);
+}
+
+
+let i = 1;
+for ( ; i <= 10; i++)
+    console.log(i)
+
+console.log('i:', i);
---
+++ JavaScript/day3/loops/loops.js
@@ -0,0 +1,7 @@
+// let sn = 1;
+// console.log(sn + ' India');
+// sn++;
+
+for (let sn = 1; sn <= 10; sn++) {
+    console.log(sn + ' India');
+}   
