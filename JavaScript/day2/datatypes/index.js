a566e7730df4a2a5c8fdff4a1d04e5dbb2d74a4e
Rakshit Mehta <rakshit0960@gmail.com>
2023-08-22 21:16:42 +0530

    added day2/basicDatypes.js 'about basic datatypes like string, numbers, objects and undefined

---
+++ JavaScript/day2/datatypes/index.js
@@ -0,0 +1,12 @@
+
+let undefinedVariable;
+let nullVariable = null;
+let str = "hello world";
+let num = 10;
+let obj = {}
+
+console.log("null Variable: ", nullVariable, typeof(nullVariable));
+console.log("UndefinedVariable: ", undefinedVariable, typeof(undefinedVariable));
+console.log("num: ", num, typeof(num));
+console.log("str: ", str, typeof(str));
+console.log("obj: ", obj, typeof(obj));
