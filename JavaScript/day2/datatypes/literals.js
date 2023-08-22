00b9ebc1019ce062a6f583a57a090c717b2996ec
Rakshit Mehta <rakshit0960@gmail.com>
2023-08-22 21:51:08 +0530

    day2/datatypes/literals.js about obj, string literals in js

---
+++ JavaScript/day2/datatypes/literals.js
@@ -0,0 +1,25 @@
+
+console.log("decimal: ", 910);
+
+console.log("octal",0o123);
+
+console.log("hexaDecimal" ,0x239ab4);
+
+console.log("binary" ,0b0011);
+
+console.log("Object literal");
+let obj1 = {name: "abc", mobile: "8978"};
+
+console.log(obj1);
+
+let obj2 = {'': 'abc', '!': '123'};
+
+console.log(obj2['']);
+console.log(obj2['!']);
+
+
+console.log('string literal');
+
+let name = 'abc';
+let greeting = `Hello git${name}!!`
+console.log(greeting);
