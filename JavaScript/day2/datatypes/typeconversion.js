184597bff8f51d1be234d2194aa2568b285d2184
Rakshit Mehta <rakshit0960@gmail.com>
2023-08-22 21:33:37 +0530

    added day2/datatypes/typeConersion.js 'about type conversion from number to string and vice versa'

---
+++ JavaScript/day2/datatypes/typeconversion.js
@@ -0,0 +1,22 @@
+let a = "Hello";
+console.log(a, typeof(a))
+
+a = 3.14;
+console.log(a, typeof(a));
+
+
+let num1 = '5.5'
+let num2 = 5;
+
+console.log(num1 + num2);
+
+console.log(parseFloat(num1) + num2);
+
+console.log(+num1, typeof(+num1));
+
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+
+let num3 = '101';
+console.log(parseInt(num3, 2))  // second argument is the base of number
