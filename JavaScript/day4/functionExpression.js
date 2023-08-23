eeac1616c499c28d9b2d698272447014fb8c2800
Rakshit Mehta <rakshit0960@gmail.com>
2023-08-23 17:39:04 +0530

    day4/fucntion expression

---
+++ JavaScript/day4/functionExpression.js
@@ -0,0 +1,16 @@
+const square = function(num) {
+    return num**2;
+}
+
+
+console.log( square(5) );
+console.log( square(4) );
+
+
+
+const getCube = function cube(num) {
+    return num**3;
+}
+
+
+console.log( getCube(3) );
