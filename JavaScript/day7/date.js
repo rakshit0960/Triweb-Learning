08ad61e8db00cc34406f2cffaf1b738213aa6f65
Rakshit Mehta <rakshit0960@gmail.com>
2023-08-25 18:18:19 +0530

    day/ date obj in js

---
+++ JavaScript/day7/date.js
@@ -0,0 +1,10 @@
+let date = "2021 07 24 12";
+console.log(date);
+
+let dateObj = new Date(date);
+
+console.log(dateObj.getMonth())
+console.log(dateObj.getFullYear())
+console.log(dateObj.getDate())
+console.log(dateObj.getTime())
+console.log(dateObj.getHours())
