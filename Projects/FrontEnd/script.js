2ef66ec893aef8b31f0b0127f94d8df43d6434de
Rakshit Mehta <rakshit0960@gmail.com>
2023-08-27 16:54:25 +0530

    added move user functionality

--- Projects/FrontEnd/script.js
+++ Projects/FrontEnd/script.js
@@ -14,18 +14,46 @@ const moveToGreenBtn = document.querySelector(".to-green")
 const moveToBlueBtn = document.querySelector(".to-blue")
 const moveToYellowBtn = document.querySelector(".to-yellow")
 
+let currentUser = ''
+
 AddUserBtn.addEventListener('click', () => {
     if (!userTextInput.value) return;
     
-    redBlock.innerText = userTextInput.value;
+    currentUser = userTextInput.value;
+    redBlock.innerText = currentUser;
     hasUser = true;
     userTextInput.placeholder = 'Cannot Add More Users'
     userTextInput.value = ''
+    AddUserBtn.disabled = true;
     userTextInput.disabled = true;
 })
 
 DelUserBtn.addEventListener('click', () => {
     boxes.forEach(box => box.innerText = '');
+    currentUser= ''
     userTextInput.placeholder = 'User Name'
     userTextInput.disabled = false;
+    AddUserBtn.disabled = false;
 })
+
+
+moveToRedBtn.addEventListener('click', () => {
+    boxes.forEach(box => box.innerText = '');
+    redBlock.innerText = currentUser;
+})
+
+moveToBlueBtn.addEventListener('click', () => {
+    boxes.forEach(box => box.innerText = '');
+    blueBlock.innerText = currentUser;
+})    
+
+moveToGreenBtn.addEventListener('click', () => {
+    boxes.forEach(box => box.innerText = '');
+    greenBlock.innerText = currentUser;
+})
+
+moveToYellowBtn.addEventListener('click', () => {
+    boxes.forEach(box => box.innerText = '');
+    yellowBlock.innerText = currentUser;
+})
+
