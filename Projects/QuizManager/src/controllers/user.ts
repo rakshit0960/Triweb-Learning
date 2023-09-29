65becee3a62b89bc2cd52e5242e7f0cce03fece4
Rakshit Mehta <rakshit0960@gmail.com>
2023-09-29 18:05:17 +0530

    genreating and sending jwt token

--- Projects/QuizManager/src/controllers/user.ts
+++ Projects/QuizManager/src/controllers/user.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { User, IUser } from '../models/user'
 import { Error } from "mongoose";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 
 interface ReturnResponse {
     status: "SUCCESS" | "ERROR",
@@ -13,7 +14,7 @@ export const registerUser = async (req: Request, res: Response) => {
     try {
         let { name, email, password } = req.body;
         password = await bcrypt.hash(password, 12);
-        const user = new User({name, email, password});
+        const user = new User({ name, email, password });
         const result = await user.save();
 
         const response: ReturnResponse = { status: "SUCCESS", message: "User Registered", data: { id: result.id } };
@@ -57,21 +58,24 @@ export const loginUser = async (req: Request, res: Response) => {
         const { email, password } = req.body;
         console.log(email, password)
 
-        const user = await User.findOne({ email }); 
+        const user = await User.findOne({ email });
         if (!user) throw new Error("user Not found!");
 
         const status = await bcrypt.compare(password, user.password.toString());
         if (!status) throw new Error("Wrong Credentials");
 
-        const response: ReturnResponse = { status: "SUCCESS", message: "Successful Logged", data: { user } }
+        const key = process.env.SECRET_KEY || "";
+        const token = jwt.sign({ userID: user.id }, key, { expiresIn: '10h' });
+
+        const response: ReturnResponse = { status: "SUCCESS", message: "Successful Logged", data: { token } }
         res.send(response);
     } catch (err: any) {
         const response: ReturnResponse = { status: "ERROR", message: "Something Went Wrong", data: { error: err } }
         if (err == Error.DocumentNotFoundError) response.message = "user Not found";
-        
+
         if (err.message == "Wrong Credentials") {
             response.message = err.message
-            res.status(401) 
+            res.status(401)
         }
 
         res.send(response);
