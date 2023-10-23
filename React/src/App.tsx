ee6c9690e9fea5ab5a4a6da6ef39753a0f775a5c
Rakshit Mehta <rakshit0960@gmail.com>
2023-10-23 19:16:57 +0530

    set up routes to homePage, contactPage and about Page using react routers

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import About from './page/About'
+import Contact from './page/Contact'
+import Home from './page/home'
 
 function App() {
-
   return (
     <>
-      <div>Hello World</div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </>
   )
 }
